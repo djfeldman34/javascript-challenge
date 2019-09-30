932fd7c4d2f8219c760773047bc34cfcae4f9c84
DJ Feldman <djfeldman92@gmail.com>
2019-09-29 23:33:36 -0400

    hw

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,36 +3,6 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 
-console.log(data);
-
-data.forEach(function(weatherReport) {
-   console.log(weatherReport);
- });
-
-data.forEach(function(weatherReport) {
-   console.log(weatherReport);
-   var row = tbody.append("tr");
- });
-
-data.forEach(function(weatherReport) {
-   console.log(weatherReport);
-   var row = tbody.append("tr");
-
-   Object.entries(weatherReport).forEach(function([key, value]) {
-     console.log(key, value);
-   });
- });
-
-data.forEach(function(weatherReport) {
-   console.log(weatherReport);
-   var row = tbody.append("tr");
-
-   Object.entries(weatherReport).forEach(function([key, value]) {
-     console.log(key, value);
-     var cell = row.append("td");
-   });
- });
-
  data.forEach(function(weatherReport) {
    console.log(weatherReport);
    var row = tbody.append("tr");
@@ -45,3 +15,21 @@ data.forEach(function(weatherReport) {
    });
  });
 
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+//  console.log(inputValue);
+//  console.log(tableData);
+
+  var filteredData = tableData.filter(weatherReport => tableData.datetime == inputValue);
+
+console.log(filteredData);
+
+});
---
+++ UFO-level-1/static/js/app_bckup01.js
@@ -0,0 +1,47 @@
+// from data.js
+var tableData = data;
+
+var tbody = d3.select("tbody");
+
+// console.log(data);
+
+// data.forEach(function(weatherReport) {
+//    console.log(weatherReport);
+//  });
+
+// data.forEach(function(weatherReport) {
+//    console.log(weatherReport);
+//    var row = tbody.append("tr");
+//  });
+
+// data.forEach(function(weatherReport) {
+//    console.log(weatherReport);
+//    var row = tbody.append("tr");
+
+//    Object.entries(weatherReport).forEach(function([key, value]) {
+//      console.log(key, value);
+//    });
+//  });
+
+// data.forEach(function(weatherReport) {
+//    console.log(weatherReport);
+//    var row = tbody.append("tr");
+
+//    Object.entries(weatherReport).forEach(function([key, value]) {
+//      console.log(key, value);
+//      var cell = row.append("td");
+//    });
+//  });
+
+ data.forEach(function(weatherReport) {
+   console.log(weatherReport);
+   var row = tbody.append("tr");
+   Object.entries(weatherReport).forEach(function([key, value]) {
+     console.log(key, value);
+     
+
+     var cell = row.append("td");
+     cell.text(value);
+   });
+ });
+
